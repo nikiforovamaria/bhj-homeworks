12dc1492be61953b0b7bbb63b74eb83b891fa318
Maria Nikiforova <nikiforovamary@gmail.com>
2020-11-30 18:31:10 +0600

    second commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,7 +5,10 @@ for (let i = 0; i < menuLink.length; i++) {
         let menuItem = menuLink[i].closest('li');
         let menuSub = menuItem.querySelector('.menu_sub');
         if (menuSub) {
-            if (menuItem.closest('ul').querySelector('.menu_active')) {
+            if (menuItem.closest('*').querySelector('.menu_active')) {
+                menuItem.closest('*').querySelector('.menu_active').classList.toggle('menu_active');
+                return false;
+            } else if (menuItem.closest('ul').querySelector('.menu_active')) {
                 menuItem.closest('ul').querySelector('.menu_active').classList.toggle('menu_active');
             }
             menuSub.classList.toggle('menu_active');
