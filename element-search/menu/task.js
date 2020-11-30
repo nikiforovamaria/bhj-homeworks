4a48061445d3d3a71ea9127cfac54b605f163e64
Maria Nikiforova <nikiforovamary@gmail.com>
2020-11-30 13:06:44 +0600

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+const menuLink = document.querySelectorAll('.menu__link');
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].onclick = function() {
+        let menuItem = menuLink[i].closest('li');
+        let menuSub = menuItem.querySelector('.menu_sub');
+        if (menuSub) {
+            if (menuItem.closest('ul').querySelector('.menu_active')) {
+                menuItem.closest('ul').querySelector('.menu_active').classList.toggle('menu_active');
+            }
+            menuSub.classList.toggle('menu_active');
+            return false;
+        }
+    } 
+}
+
