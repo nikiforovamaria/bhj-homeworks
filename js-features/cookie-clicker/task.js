e58799d88f2e9c3cfb11098f73b13354741996c8
Maria Nikiforova <nikiforovamary@gmail.com>
2020-11-30 18:11:46 +0600

    third commit cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,18 +1,19 @@
 //Задача 2.1
 const clicker = document.getElementById('clicker__counter');
 const image = document.getElementById('cookie');
+let timeBefore = new Date();
 
 image.onclick = function() {
-    const timeBefore = new Date();
+    const timeAfter = new Date();
     if (image.width == 200) {
         image.width = 300;
     } else if (image.width == 300) {
         image.width = 200;
     }
     clicker.textContent++;
-    const timeAfter = new Date();
-    const speed = 1 /((timeAfter.getTime() - timeBefore.getTime())*60);
-    clickSpeed.textContent = speed;
+    const speed = 1 / ((timeAfter - timeBefore) / 1000);
+    clickSpeed.textContent = speed.toFixed(2);
+    timeBefore = new Date();
     return true;
 }
 
