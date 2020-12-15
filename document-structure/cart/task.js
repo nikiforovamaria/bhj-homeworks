d6b49d9b77b1cb434a2e934c1596b53cdb060fd2
Maria Nikiforova <nikiforovamary@gmail.com>
2020-12-15 21:31:52 +0600

    first commit cart

---
+++ document-structure/cart/task.js
@@ -0,0 +1,36 @@
+const cartProducts = document.querySelector('.cart__products');
+const product = document.querySelectorAll('.product');
+const controls = document.querySelectorAll('.product__quantity-control');
+const buttonAdd = document.querySelectorAll('.product__add');
+
+for (let item of controls) {
+    item.addEventListener('click', () => {
+        let value = item.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+        if (item.classList.contains('product__quantity-control_dec')) {
+            value.textContent--;
+            if (value.textContent < 1) {
+                value.textContent = 1;
+            }
+        } else if (item.classList.contains('product__quantity-control_inc')) {
+            value.textContent++;
+        }
+    })
+}
+
+for (let item of buttonAdd) {
+    item.addEventListener('click', () => {
+        const id = item.closest('.product').dataset.id;
+        const count = Number(item.closest('.product').querySelector('.product__quantity-value').textContent);
+        const img = item.closest('.product').querySelector('img').getAttribute('src');
+        
+        if (cartProducts.querySelector(`[data-id = '${id}']`)) {
+            const num = Number(cartProducts.querySelector(`[data-id = '${id}']`).querySelector('.cart__product-count').textContent);
+            cartProducts.querySelector(`[data-id = '${id}']`).querySelector('.cart__product-count').textContent = num + count;
+        } else {
+            cartProducts.innerHTML += `<div class="cart__product" data-id="${id}">
+                <img class="cart__product-image" src="${img}">
+                <div class="cart__product-count">${count}</div>
+                </div>`;
+        }
+    });
+}
