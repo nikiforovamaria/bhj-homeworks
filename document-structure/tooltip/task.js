920f40c158a63d9381434698f5befcfdeb1a7c5b
Maria Nikiforova <nikiforovamary@gmail.com>
2020-12-16 19:59:57 +0600

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,12 +1,19 @@
 const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
 
 for (let item of tooltips) {
     item.addEventListener('click', (e) => {
         e.preventDefault();
-        if (document.querySelector('.tooltip')) {
-            document.querySelector('.tooltip').remove();
+        item.appendChild(tooltip);
+        const activeTip = document.querySelector('.tooltip_active');
+        if (tooltip.contains(activeTip) && activeTip.textContent == item.getAttribute('title')) {
+            tooltip.classList.remove('tooltip_active');
+        } else {
+            tooltip.classList.add('tooltip_active');
         }
-        const title = item.getAttribute('title');
-        item.insertAdjacentHTML('afterend', `<div class="tooltip" style="display: block">${title}</div>`);
-    })
+        tooltip.innerHTML = item.getAttribute('title');
+        tooltip.style.left = item.getBoundingClientRect().left +'px';
+        tooltip.style.top = item.getBoundingClientRect().bottom +'px';
+    });
 }
