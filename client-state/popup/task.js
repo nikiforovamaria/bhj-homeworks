8c10359d069ab01ef12517f59ba5866779fd1b65
Maria Nikiforova <nikiforovamary@gmail.com>
2020-12-20 20:03:56 +0600

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById('subscribe-modal');
+const modalClose = document.querySelector('.modal__close');
+
+window.addEventListener('load', () => {
+    if (document.cookie = '') {
+        modal.classList.add('modal_active');
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+    document.cookie = "modal=close";
+});
